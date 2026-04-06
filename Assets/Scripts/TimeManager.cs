b38b0bb0ca5d0b00255923bc67860fc31d72ce79
DevNoteKeeper <seoyoon.developer@gmail.com>
2026-04-06 17:07:12 +0200

    create time function
    current 1 day is 15 minutes, it can change using Real Minutes Per Day

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,105 @@
+using System;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    [SerializeField]
+    private float realMinutesPerDay = 15f;
+    private float RealSecondsPerGameMinutes => (realMinutesPerDay * 60f) / 1440f;
+
+    public int currentMonth;
+    public int currentDay;
+    public int currentHour;
+    public int currentMinute;
+
+    private float timer;
+    private bool isRunning;
+
+    private bool isNight;
+
+    private float mealTimer = 0f;
+    private bool isMealTime;
+    [SerializeField]
+    private float mealDurationRealSecond = 60f;
+
+    private void Start()
+    {
+        StartTime();
+    }
+    private void Update()
+    {
+        if (!isRunning) return;
+        timer += Time.deltaTime;
+
+        if (timer >= RealSecondsPerGameMinutes)
+        {
+            timer -= RealSecondsPerGameMinutes;
+            AdvanceOneMinute();
+         
+        }
+        MealTimer();
+    }
+    public void StartTime(int month = 7, int day = 1)
+    {
+        currentMonth = month;
+        currentDay = day;
+        currentHour = 7;
+        currentMinute = 0;
+        isRunning = true;
+    }
+
+    private void AdvanceOneMinute()
+    {
+        currentMinute++;
+
+        if(currentMinute >= 60)
+        {
+            currentMinute = 0;
+            currentHour++;
+            CheckNightTime();
+        }
+        if (currentHour  >= 24)
+        {
+            currentHour = 0;
+            DateTime current = new DateTime(2026, currentMonth, currentDay);
+            DateTime next = current.AddDays(1);
+
+            currentMonth = next.Month;
+            currentDay = next.Day;
+        }
+    }
+
+    public bool MealTimer()
+    {
+        TimeSpan breakfast = new TimeSpan(7, 0, 0);
+        TimeSpan lunch = new TimeSpan(12, 0, 0);
+        TimeSpan dinner = new TimeSpan(18, 0, 0);
+        TimeSpan currentTime = new TimeSpan(currentHour, currentMinute, 0);
+
+        // start meal time
+        if(!isMealTime && (currentTime == breakfast || currentTime == lunch || currentTime == dinner))
+        {
+            isMealTime = true;
+            mealTimer = 0f;
+        }
+
+        // operate mealTimer
+        if (isMealTime)
+        {
+            mealTimer += Time.deltaTime;
+            if(mealTimer >= mealDurationRealSecond)
+            {
+                isMealTime= false;
+            }
+        }
+        
+        return isMealTime;
+    }
+
+    private void CheckNightTime()
+    {
+        isNight = (currentHour >= 21 || currentHour < 7);
+    }
+
+
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private TimeManager timeManager;
+    [SerializeField]
+    private TMP_Text dateText;
+    [SerializeField]
+    private TMP_Text timeText;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        dateText.text = $"{timeManager.currentMonth}/{timeManager.currentDay}";
+        timeText.text = $"{timeManager.currentHour:D2} : {timeManager.currentMinute:D2}";
+    }
+}
