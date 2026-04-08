a03914a08bdf525f90358c5039ef9bd552ebda20
DevNoteKeeper <seoyoon.developer@gmail.com>
2026-04-08 06:40:28 +0200

    create Start Scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
 
+public enum GameState
+{
+    Egg,
+    Playing,
+    Sleeping,
+    Dead
+}
+
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private StatsManager statsManager;
+    [SerializeField] private TimeManager timeManager;
+    [SerializeField] private SleepManager sleepManager;
+    [SerializeField] private DeathManager deathManager;
+
+    public GameState currentState = GameState.Egg;
+
+    public void StartGame(int month, int day)
     {
-        
+        currentState = GameState.Playing;
+        timeManager.StartTime(month, day);
+        Debug.Log($"Game Start {month}/{day}");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RestartGame()
     {
-        
+        currentState = GameState.Egg;
+        statsManager.ResetStats();
+        Debug.Log("Game Reset");
     }
+
+    private void Start()
+    {
+        string name = PlayerPrefs.GetString("name");
+        int month = PlayerPrefs.GetInt("month");
+        int day = PlayerPrefs.GetInt("day");
+
+        Debug.Log($"Creature: {name}, {month}/{day}");
+        StartGame(month, day);
+
+    }
+
 }
