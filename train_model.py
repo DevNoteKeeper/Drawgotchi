0cce17d09b656ec4494c64b66aa50450fc38b1ab
Pringles <80323633+PringlePot@users.noreply.github.com>
2026-04-09 15:54:23 +0200

    More drawings (model got worse??)

--- train_model.py
+++ train_model.py
@@ -1,175 +1,92 @@
-# Training section of model
 import tensorflow as tf
+from tensorflow import keras
 
-
-import keras_tuner as kt
-
-# define augmentation layers to apply random transformations
-# define as global variable to avoid recreating on every map call
-augment_layer = tf.keras.Sequential([
-        # Left/Right inversion and horizontal flip
-        tf.keras.layers.RandomFlip("horizontal_vertical"),
-        # +-30% rotation
-        tf.keras.layers.RandomRotation(0.15),
-        # +-20% zoom in/out
-        tf.keras.layers.RandomZoom(0.2),
-    ])
-
-
-# Add original and multiple augmented versions of the same image to our dataset
-def data_augmentation(img, label):
-    augmented_list = [img]
-    for _ in range(3):
-        augmented_list.append(augment_layer(img, training=True))
-
-    augmented_images = tf.concat(augmented_list, axis=0)
-    repeated_labels = tf.concat([label] * 4, axis=0)
-    return augmented_images, repeated_labels
-
+image_size = (480,310)
 
 def load_data():
     train_ds = tf.keras.utils.image_dataset_from_directory(
-        'dataset',
-        label_mode='categorical',
-        image_size=(240, 155),
-        validation_split=0.3,
-        batch_size=16,
-        subset='training',
-        shuffle=True,
-        seed=42,
-        color_mode="grayscale"
+        "dataset",
+        validation_split=0.2,
+        subset="training",
+        seed=123,
+        image_size=image_size,
+        batch_size=32,
+        color_mode="grayscale",
     )
 
-    remain_ds = tf.keras.utils.image_dataset_from_directory(
-        'dataset',
-        label_mode='categorical',
-        image_size=(240, 155),
-        validation_split=0.3,
-        batch_size=16,
-        subset='validation',
-        shuffle=True,
-        seed=42,
-        color_mode="grayscale"
+    val_ds = tf.keras.utils.image_dataset_from_directory(
+        "dataset",
+        validation_split=0.2,
+        subset="validation",
+        seed=123,
+        image_size=image_size,
+        batch_size=32,
+        color_mode="grayscale",
     )
 
-    # Apply augmentation with originals
-    train_ds = train_ds.map(data_augmentation)
+    AUTOTUNE = tf.data.AUTOTUNE
+
+    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+    return train_ds, val_ds
+
+def build_model(num_classes):
+    model = keras.Sequential([
+        keras.layers.Input(shape=(image_size[0], image_size[1], 1)),
+        keras.layers.Rescaling(1./255),
+
+        # keras.layers.RandomFlip("horizontal"),
+        # keras.layers.RandomRotation(0.2),  # ±20% rotation
+        # keras.layers.RandomZoom(0.2),      # ±20% zoom
+        keras.layers.RandomTranslation(0.2, 0.2),  # Shifts (random movement)
 
 
-    # split remain_images into val_ds and test_ds by batch count
-    remain_card = remain_ds.cardinality().numpy()
-    val_ds = remain_ds.take(remain_card // 2)
-    test_ds = remain_ds.skip(remain_card // 2)
-
-    return train_ds, val_ds, test_ds
-
-
-
-# build cnn model with tunable hyperparameters
-def build_model(hp):
-    model = tf.keras.Sequential([
-        # to extract image feature step by step
-        # example: line/edge -> pattern -> ear shape/muffin shape
-        # filter, [16,32, 64]: to detect simple to complex feature used 16-64
-        tf.keras.layers.Conv2D(hp.Choice('filter_1', [16, 32, 64]), 3, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)),
-        # randomly disable 30% of neurons to prevent overfitting
-        tf.keras.layers.Dropout(0.3),
-        tf.keras.layers.MaxPooling2D(),
-        tf.keras.layers.Conv2D(hp.Choice('filter_2', [16, 32, 64]), 3, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)),
-        tf.keras.layers.Dropout(0.3),
-        tf.keras.layers.MaxPooling2D(),
-        tf.keras.layers.Conv2D(hp.Choice('filter_3', [16, 32, 64, 128]), 3, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)),
-        tf.keras.layers.Dropout(0.3),
-        tf.keras.layers.MaxPooling2D(),
-
-        # 2d to 1d to use dense
-        tf.keras.layers.Flatten(),
-        # combine feature
-        # units, [64, 128]: to avoid overfitting with small dataset used 64-128
-        tf.keras.layers.Dense(hp.Choice('units_1', [64, 128]), activation='relu'),
-
+        keras.layers.Conv2D(16, 3, padding='same', activation='relu'),
+        keras.layers.MaxPooling2D(),
+        keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
+        keras.layers.MaxPooling2D(),
+        keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
+        keras.layers.MaxPooling2D(),
+        keras.layers.Flatten(),
+        keras.layers.Dense(256, activation='relu'),
+        keras.layers.Dense(num_classes)
     ])
 
-    second_layer = hp.Choice('units_2', [0, 64, 128])
-    if second_layer > 0:
-        model.add(tf.keras.layers.Dense(second_layer, activation='relu')),
-
-    # final classification
-    model.add(tf.keras.layers.Dense(6, activation="softmax"))
-    # learning_rate, [0.01, 0.001, 0.0001]: to process aggressive to fine-tuned training
-    model.compile(
-        optimizer=tf.keras.optimizers.Adam(hp.Choice('learning_rate', [0.01, 0.001, 0.0001])),
-        loss='categorical_crossentropy',
-        metrics=['accuracy'])
-
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001),
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
     return model
 
-# find the best optimization parameter using random search
-def optimize_parameter(train_ds, val_ds):
-    tuner = kt.RandomSearch(
-        hypermodel = build_model,
-        objective='val_accuracy',
-        max_trials=10,
-        project_name="doodle_detection"
-    )
-    tuner.search(
+def train_model(model, train_ds, val_ds):
+    model.fit(
         train_ds,
         validation_data=val_ds,
-        epochs=10,
+        epochs=30
     )
-    # return best parameter [0] -> 1st place
-    return tuner.get_best_hyperparameters()[0]
+
+def confusion_matrix(model, val_ds):
+    y_true = []
+    y_pred = []
+
+    for images, labels in val_ds:
+        predictions = model.predict(images)
+        predicted_labels = tf.argmax(predictions, axis=1)
+        y_true.extend(labels.numpy())
+        y_pred.extend(predicted_labels.numpy())
+
+    cm = tf.math.confusion_matrix(y_true, y_pred)
+    print("Confusion Matrix:")
+    print(cm)
 
 
-# train data and monitor performance with validation data
-def train_model(train_ds, val_ds, best_hp):
-    model = tf.keras.Sequential([
-        tf.keras.layers.Conv2D(best_hp.get('filter_1'), 3, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)),
-        tf.keras.layers.Dropout(0.3),
-        tf.keras.layers.MaxPooling2D(),
-        tf.keras.layers.Conv2D(best_hp.get('filter_2'), 3, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)),
-        tf.keras.layers.Dropout(0.3),
-        tf.keras.layers.MaxPooling2D(),
-        tf.keras.layers.Conv2D(best_hp.get('filter_3'), 3, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)),
-        tf.keras.layers.Dropout(0.3),
-        tf.keras.layers.MaxPooling2D(),
+def main():
+    train_ds, val_ds = load_data()
+    model = build_model(6)
+    train_model(model, train_ds, val_ds)
+    confusion_matrix(model, val_ds)
 
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(best_hp.get('units_1'), activation='relu'),
+    model.save("model.keras")
 
-    ])
-
-    second_layer = best_hp.get('units_1')
-    if second_layer > 0:
-        tf.keras.layers.Dense(second_layer, activation='relu'),
-
-
-    model.add(tf.keras.layers.Dense(6, activation="softmax"))
-
-    model.compile(
-        optimizer=tf.keras.optimizers.Adam(best_hp.get('learning_rate')),
-        loss='categorical_crossentropy',
-        metrics=['accuracy'])
-
-
-    hist = model.fit(
-        train_ds,
-        validation_data=val_ds,
-        callbacks=[tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)],
-        epochs=30)
-
-    return model, hist
-
-# Training and optimization process
-def do_training():
-    train_ds, val_ds, test_ds = load_data()
-    best_hp = optimize_parameter(train_ds, val_ds)
-    model, history = train_model(train_ds, val_ds, best_hp)
-
-    model.save('model.keras')
-
-    return model, history, test_ds
-
-# Allow you to only run training
-if __name__ == '__main__':
-    do_training()
+if __name__ == "__main__":
+    main()
